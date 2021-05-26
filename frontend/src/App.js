6f2741869bb8e5a44fb2cafda0d93b3acfa30224
Logan Negley <logannegley@gmail.com>
2021-05-26 11:00:46 -0500

    reverted from bootstrap for now---installed styled components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,11 @@
-import react from 'react';
+import React from 'react';
+import Header from './components/Header';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        My app
-      </header>
-    </div>
+    <>
+      <Header/>
+    </>
   );
 }
 
